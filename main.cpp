b257b2c900e06bde67ebf382b7e78acc107cebe6
BruceMu <morningmcq@163.com>
2014-06-16 20:56:46 +0800

    aaa

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "vol_math_ImageVolume.h"
+#include "vol_math_filter_Interface.h"
+#include "vol_math_RawImage.h"
+int main(){
+    int wid = 281,hei=481,dep =301;
+    int len = wid*hei*dep;
+    RawImage rawtest;
+    unsigned char * indata = new unsigned char[len];
+  //  unsigned char * outdata = new unsigned char[len];
+    rawtest.readImage(indata,"/home/mu/mig.raw",len*sizeof(unsigned char));
+    ImageVolume * src = new ImageVolume(wid,hei,dep,1,indata);
+    ImageVolume * ret = new ImageVolume(wid,hei,dep,1,true);
+    lowPassI p(100);
+    dolowPassI(src,ret,p);
+    delete [] indata;
+    delete src;
+    rawtest.writeImagesesmicarray(ret->Data,wid,hei,dep);
+    delete ret;
+
+}
+/*
+int main()
+{
+    testFftinterface();
+    return 0;
+}
+*/
---
+++ vol_math_filter_Interface.cpp
@@ -0,0 +1,24 @@
+#include "vol_math_filter_Interface.h"
+#include "vol_math_FourierFilter3.h"
+//qym
+#include "vol_math_RawImage.h"
+using namespace std;
+extern size_t  globalProgressChanged = 0;
+extern size_t volatile progressStep = 0;
+void progress(int type,int total ,int step,bool &cancled)
+{
+
+	printf(" %f\n",(float)step*100/total);
+}
+
+ void  dolowPassI (ImageVolume *src,ImageVolume * ret,lowPassI &para)
+{
+	Raw  indata = *(Raw *)ImageVolume2Raw(src);
+	FourierFilter3 * ff3 = new FourierFilter3(indata);
+	ff3->lowpass_trans(para.threshold,progress);
+	ImageVolume * res = (ImageVolume*)Raw2ImageVolume(*(ff3->fraw),ret->PixelType);
+	memcpy(ret->Data,res->Data,ff3->fraw->size()*sizeof(unsigned char));
+	delete res;
+	delete ff3;
+//	return ret;
+}
